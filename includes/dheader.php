e9de19be76f65871197a6fb98f75de1e59bf2c00
Damzaky <dmzsylvester@gmail.com>
2018-01-06 16:09:48 +0700

    navleft structure change

--- includes/dheader.php
+++ includes/dheader.php
@@ -4,7 +4,7 @@ Genwis
 </a>
 <div class='widg' id='utam'>
 <h4>MENU</h4>
-<nav class="nav flex-column">
+<nav class="nav flex-column utama">
   <a class="nav-link <?php if($dsb==true){echo "actived";}?>" href="./dmain.php"><img src="<?php if($dsb==true){echo "./icon/active/dashboard.png";}else{echo "./icon/noactive/dashboard.png";} ?>"/> Dashboard</a>
   <a class="nav-link <?php if($atr==true){echo "actived";}?>" href="./dattraction.php"><img src="<?php if($atr==true){echo "./icon/active/international-delivery-1.png";}else{echo "./icon/noactive/international-delivery-1.png";} ?>"/> Attraction</a>
   <a class="nav-link <?php if($mng==true){echo "actived";}?>" href="./dmanage.php"><img src="<?php if($mng==true){echo "./icon/active/user-2.png";}else{echo "./icon/noactive/user-2.png";} ?>"/> Manage User</a>
@@ -13,7 +13,7 @@ Genwis
 </div>
 <div class='widg'>
 <h4>OTHERS</h4>
-<nav class="nav flex-column">
+<nav class="nav flex-column secondm">
   <a class="nav-link" href="#">Need Help?</a>
   <a class="nav-link" href="#">How it Works</a>
   <a class="nav-link" href="#">Learn More</a>
