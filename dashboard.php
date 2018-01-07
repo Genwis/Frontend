cf5154fcc46f35fcc78a175a0ad96843b70681e7
Damzaky <dmzsylvester@gmail.com>
2018-01-07 17:12:17 +0700

    dashboard minus statistics

--- dashboard.php
+++ dashboard.php
@@ -63,6 +63,41 @@ include ('includes/dheader.php');?>
 </div>
 <div class='widg'>
 <h4>Latest Users</h4>
-
+<div class='kotaq'>
+<table class="table latestu">
+  <thead>
+    <tr>
+      <th scope="col">E-mail</th>
+      <th scope="col">Budget</th>
+      <th scope="col">City Destination</th>
+      <th scope="col">Start</th>
+	  <th scope="col">End</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Tsubasaozora13@gmail.com</td>
+      <td>IDR 10000000</td>
+      <td>Bandung, West Java</td>
+      <td>27 May 2017</td>
+	  <td>03 June 2017</td>
+    </tr>
+    <tr>
+      <td>Tsubasaozora13@gmail.com</td>
+      <td>IDR 10000000</td>
+      <td>Bandung, West Java</td>
+      <td>27 May 2017</td>
+	  <td>03 June 2017</td>
+    </tr>
+    <tr>
+      <td>Tsubasaozora13@gmail.com</td>
+      <td>IDR 10000000</td>
+      <td>Bandung, West Java</td>
+      <td>27 May 2017</td>
+	  <td>03 June 2017</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </div>
 <?php include ('includes/dfooter.php'); ?>
