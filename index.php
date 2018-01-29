895fbdae6ee4e0f31d4b96640cf371aef253c53d
Damzaky <dmzsylvester@gmail.com>
2018-01-29 22:35:28 +0700

    update index

--- index.php
+++ index.php
@@ -14,17 +14,20 @@
 <div class="full">
 <div class='bag' id='opener'>
 <nav id='barnav'>
-<div class='juds'>Genpart</div>	<div class='menus'><a href='#' class='m'>How it works</a> <a href='#' class='m'>Features</a> <a href='#' class='m'>About</a><a class='signin' href='#'>Sign in</a></div> 
+<div class='juds'>Genpart</div>	<div class='menus'><a href='#howitworks' class='m'>How it works</a> <a href='#features' class='m'>Features</a> <a href='#' class='m'>About</a><a class='signin' href='#'>Sign in</a></div> 
 </nav>
 <div class='ghost'>
 <div class='daleman'>
 
 
 <br/>
-<h1>Genwis Partner for growth local agent or local partner</h1>
+<h1><span class='gpart'>Genwis Partner</span> for growth local agent or local partner</h1>
 <br/>
+<p>
 Genwis Partner is platform for local partner or local travel agent that would help to growth tour business
+<br/>
 in the some city with three powerfull fiture is view all statistic, manage user and manage attraction.
+</p>
 <br/>
 <!--<a class='btn btn-primary'>Get Started</a>-->
 <a href='#' class='getstart'>Get Started</a>
@@ -35,46 +38,31 @@ in the some city with three powerfull fiture is view all statistic, manage user 
 
 
 
-<div class='micin part1'>
-
-
+<div class='micin' id='howitworks'>
+<h2 class='howi'>How it works</h2>
+<hr/>
 </div>
-<div class='micin part2'>
-What do you think?
-<br/>
-Genwis<br/>
-Genwis adalah sebuah aplikasi mobile yang sangat meng-genrate itinerar wisata atau dapat mengatur jadwal wisata user setiap harinya sesuai dengan kata tujuan, budget, dan jangka waktu yang user tentukan berdasarkan aplikasi Genwis
+<div class='micin' id='features'>
+<h2>Growth your business with 3 powerful features</h2>
+Genwis Partner have three powerfull fiture for local agent or
+local partner that could promote your business.
+<h3>View All Statistic</h3>
+You could see all of statistic on this website about your 
+revenue, active user, statis revenue per-month etc. 
+<h3>Manage Attraction</h3>
+You could manage your attraction that you can services
+our user in the city.
+<h3>Manage User</h3>
+You could manage your user that booked with your
+services example edit or delete that user data.
 </div>
-<!--<div class='micin part3'>
-What Will You Get?
-<br/>
-  <div class="row">
-    <div class="col-sm">
-      Generate
-	  <br/>
-	  Anda dapat melihat itinerary wisatanya sesuai dengan budget, kota tujuan, jangka waktu yang anda tentukan berdasarkan pada aplikasi Genwis
-    </div>
-    <div class="col-sm">
-      Booking
-	  <br/>
-	  Anda dapat memesan tempat-tempat wisata yang telah anda setujui melalui perantara aplikasi Genwis
-    </div>
-    <div class="col-sm">
-      Change
-	  <br/>
-	  Anda dapat mengubah tempat atau jangka waktu kunjungan dari setiap tempat-tempat yang direkomendasikan oleh aplikasi Genwis
-    </div>
-  </div>
-</div>-->
+<div class='micin'>
+<h2>Type your email here for more information from us</h2>
+<div class='modalb'>
+Type your email address here <a href='#'>Send</a>
+</div>
+</div>
 
-
-
-<h1>h1 Bootstrap heading (36px)</h1>
-<h2>h2 Bootstrap heading (30px)</h2>
-<h3>h3 Bootstrap heading (24px)</h3>
-<h4>h4 Bootstrap heading (18px)</h4>
-<h5>h5 Bootstrap heading (14px)</h5>
-<h6>h6 Bootstrap heading (12px)</h6>
 <?php //include ('includes/footer.php'); ?>
 
 </div>
